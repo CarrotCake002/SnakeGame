e0ebfd6f63d7946dcc8601d0a17f3d4a74414f0f
CarrotCake <pol.siles-poloepitech.eu>
2025-08-03 21:29:07 +0200

    [ADD] loading of the assets, but assets are mistakenly 1600x1600 instead of the intended 16x16 and need to be reuploaded

--- include/Render.hpp
+++ include/Render.hpp
@@ -5,13 +5,29 @@
 #include <memory>
 #include "Game.hpp"
 
+struct SnakeTextures {
+    sf::Texture snakeHead;
+    sf::Texture snakeStraightBody;
+    sf::Texture snakeRightBody;
+    sf::Texture snakeLeftBody;
+    sf::Texture snakeTail;
+};
+
 class Render {
     public:
         Render(sf::RenderWindow *window);
         ~Render();
 
+        void loadAssets(void);
+        void drawBackground(int rowsSize, int colsSize);
+        void drawGrid(int rowsSize, int colsSize);
+        void drawSnake(void);
         void display(Game *game);
     private:
+        sf::Texture grass;
+        sf::Texture fruit;
+        sf::Texture wall;
+        SnakeTextures snakeTextures;
         sf::RenderWindow *window;
 };
 
--- src/Render.cpp
+++ src/Render.cpp
@@ -2,10 +2,57 @@
 
 Render::Render(sf::RenderWindow *window) {
     this->window = window;
+    this->loadAssets();
 }
 
 Render::~Render() {
 
+}
+
+void Render::loadAssets(void) {
+    if (!this->grass.loadFromFile("../assets/background_grass_light.png")) {}
+    if (!this->fruit.loadFromFile("../assets/fruit.png")) {}
+    if (!this->wall.loadFromFile("../assets/background_grass_dense.png")) {}
+    if (!this->snakeTextures.snakeHead.loadFromFile("../assets/snake_head.png")) {}
+    if (!this->snakeTextures.snakeStraightBody.loadFromFile("../assets/snake_body_straight.png")) {}
+    if (!this->snakeTextures.snakeLeftBody.loadFromFile("../assets/snake_body_left.png")) {}
+    if (!this->snakeTextures.snakeRightBody.loadFromFile("../assets/snake_body_right.png")) {}
+    if (!this->snakeTextures.snakeTail.loadFromFile("../assets/snake_tail.png")) {}
+}
+
+void Render::drawBackground(int rowsSize, int colsSize) {
+    sf::Sprite grassTile;
+
+    this->grass.setSmooth(false);
+    grassTile.setTexture(this->grass);
+
+    for (int row = 0; row < rowsSize; row++) {
+        for (int col = 0; col < colsSize; col++) {
+            grassTile.setPosition(col * 16.0f * 2, row * 16.0f * 2);
+            window->draw(grassTile);
+        }
+    }
+}
+
+void Render::drawGrid(int rowSize, int colSize) {
+    sf::Color gridColor(100, 100, 100, 100);  // light gray, semi-transparent
+    sf::RectangleShape line;
+
+    // Vertical lines
+    for (int x = 0; x <= colSize; ++x) {
+        line.setSize(sf::Vector2f(1.f, rowSize * 16 * 2));
+        line.setPosition(x * 16.f * 2, 0.f);
+        line.setFillColor(gridColor);
+        window->draw(line);
+    }
+
+    // Horizontal lines
+    for (int y = 0; y <= rowSize; ++y) {
+        line.setSize(sf::Vector2f(colSize * 16 * 2, 1.f));
+        line.setPosition(0.f, y * 16.f * 2);
+        line.setFillColor(gridColor);
+        window->draw(line);
+    }
 }
 
 void Render::display(Game *game) {
@@ -15,6 +62,8 @@ void Render::display(Game *game) {
         }
         std::cout << std::endl;
     }
-    //window->clear(sf::Color::Black);
-    //window->display();
+    window->clear(sf::Color::Black);
+    this->drawBackground(game->map.size(), game->map[0].size());
+    this->drawGrid(game->map.size(), game->map[0].size());
+    window->display();
 }
